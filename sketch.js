4aea10a34286f276b0eae1732427a7827970af2e
ETan139 <88403242+ETan139@users.noreply.github.com>
2021-08-03 15:45:40 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var ball 
+function setup() {
+  createCanvas(400,400);
+ball=createSprite(200,200,10,10)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+if (keyDown("left")) {
+ball.velocityX=-10
+}
+if (keyDown("right")) {
+ball.velocityX=10
+}
+if (keyDown(UP_ARROW)) {
+ball.velocityY=-10
+}
+if (keyDown(DOWN_ARROW)) {
+  ball.velocityY=10
+}
+}
+
+
+
+
+
